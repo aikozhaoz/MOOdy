d81419de4c6893e37a64c3206fad006a4e04ea24
Thomas Postak <braves7122@hotmail.com>
2022-04-08 18:52:59 -0400

    added journal.java

---
+++ app/src/main/java/com/mentalhealthapp/moody/Journal.java
@@ -0,0 +1,50 @@
+package com.mentalhealthapp.moody;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+public class Journal {
+    String date;
+    String journalTitle;
+    String journalText;
+
+
+    public Journal(){
+
+    }
+
+    public Journal(String journalTitle, String journalText) {
+        this.journalText = journalText;
+        this.journalTitle = journalTitle;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = Calendar.getInstance();
+        this.date = sdf.format(c.getTime());
+    }
+
+
+    public void setJournalTitle(String journalTitle){
+        this.journalTitle = journalTitle;
+    }
+
+    public String getJournalTitle(){
+        return this.journalTitle;
+    }
+
+    public void setJournalText(String journalText){
+        this.journalText = journalText;
+    }
+
+    public String getJournalText(){
+        return this.journalText;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+}
