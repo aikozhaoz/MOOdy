77883c32398c1f15af7a742bd9d4b9854a54e94c
Thomas Postak <braves7122@hotmail.com>
2022-04-08 18:38:14 -0400

    Added Aikos changes for journal fragment and layout

--- app/src/main/java/com/mentalhealthapp/moody/JournalFragment.java
+++ app/src/main/java/com/mentalhealthapp/moody/JournalFragment.java
@@ -1,64 +1,109 @@
 package com.mentalhealthapp.moody;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link JournalFragment#newInstance} factory method to
+ * Use the {@link JournalFragment#} factory method to
  * create an instance of this fragment.
  */
 public class JournalFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public JournalFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static JournalFragment newInstance(String param1, String param2) {
-        JournalFragment fragment = new JournalFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private DatabaseReference db;
+    private String userID;
+    View journalFragmentView;
+    Boolean journalFilled = false;
+    EditText etJournalTitle, etJournalText;
+    Journal journal;
+    ImageView previewImg;
+    Button uploadJournalPicture, reviewJournals, submitButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_journal, container, false);
+    }
+
+//    ActivityResultLauncher<Intent> launchSelectImage = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+//        @Override
+//        public void onActivityResult(Uri result) {
+//
+//        }
+//    });
+//
+//    public void selectImage(){
+//        Intent iGallery = new Intent(Intent.ACTION_PICK);
+//        iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(iGallery, GALLERY_REQ_CODE);
+//        selectImageIntent.setType("image/*");
+//        selectImageIntent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(Intent.createChooser(selectImageIntent, "Upload Image"), 200);
+//    }
+
+    public void createJournal(String journalTitle, String journalText){
+        journal = new Journal();
+        journal.setJournalTitle(journalTitle);
+        journal.setJournalText(journalText);
+    }
+
+    public void writeJournalDB(){
+
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Initialize firebase database
+        userID = FirebaseAuth.getInstance().getUid();
+        db = FirebaseDatabase.getInstance().getReference();
+
+        // Get current view
+        journalFragmentView = view;
+
+        // Get journal title and text
+        etJournalTitle = journalFragmentView.findViewById(R.id.etjournal_title);
+        etJournalText = journalFragmentView.findViewById(R.id.etjournal_text);
+
+        // Initialize all the buttons
+        reviewJournals = (Button) journalFragmentView.findViewById(R.id.review_journals);
+        submitButton = (Button) journalFragmentView.findViewById(R.id.submit_journal);
+
+        // Get the preview image
+//        uploadJournalPicture.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                launchSelectImage.launch("image/*");
+//            }
+//        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i = 2;
+                String journalTitle = etJournalTitle.getText().toString();
+                String journalText = etJournalText.getText().toString();
+                createJournal(journalTitle,journalText);
+            }
+        } );
     }
 }
