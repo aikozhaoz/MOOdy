fa09d9d1ded7ea0f83fab73ec1c223aa0d47cee9
Thomas Postak <braves7122@hotmail.com>
2022-04-07 23:23:05 -0400

    Added working goals fragment

--- app/src/main/java/com/mentalhealthapp/moody/GoalsFragment.java
+++ app/src/main/java/com/mentalhealthapp/moody/GoalsFragment.java
@@ -1,12 +1,34 @@
 package com.mentalhealthapp.moody;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +36,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GoalsFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private EditText goalText;
+    FirebaseUser user;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +54,7 @@ public class GoalsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private EditText input;
     public GoalsFragment() {
         // Required empty public constructor
     }
@@ -53,6 +84,97 @@ public class GoalsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Goals/"+user.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                GoalsData value = snapshot.getValue(GoalsData.class);
+                if (value != null){
+                    List<String> valueList = value.getGoals();
+                    updateGoalList(valueList);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        String uid = user.getUid();
+        //DatabaseReference ref = database.getReference();
+
+        //ref.orderByChild("email").equalTo(email);
+        //ref = database.getReference("Users" + user.getFirstName() + user.getLastName());
+        input = getView().findViewById(R.id.editGoalText);
+        listView = view.findViewById(R.id.goalsView);
+        button = view.findViewById(R.id.addGoalButton);
+        goalText = (EditText) view.findViewById(R.id.editGoalText);
+        goalText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (input.getText().equals("Enter Goal")){
+                    input.getText().clear();
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+
+
+
+
+        setUpListViewListener();
+
+
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getActivity().getApplicationContext();
+                Toast.makeText(context, "Goal Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+
+                updateDatabase();
+
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            input.setText("");
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter text...", Toast.LENGTH_LONG).show();
+        }
+
+        updateDatabase();
+
+
     }
 
     @Override
@@ -60,5 +182,22 @@ public class GoalsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_goals, container, false);
+    }
+
+    public void updateDatabase() {
+        List<String> goalList = new ArrayList<>();
+        for (int i=0; i<itemsAdapter.getCount(); i++){
+            goalList.add((String) itemsAdapter.getItem(i));
+        }
+
+        GoalsData goalData = new GoalsData(goalList);
+        databaseReference.setValue(goalData);
+    }
+
+    public void updateGoalList(List<String> goalList){
+        itemsAdapter.clear();
+        for (int i=0; i< goalList.size(); i++){
+            itemsAdapter.add(goalList.get(i));
+        }
     }
 }
