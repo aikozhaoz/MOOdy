7986630be7fb2bd85456a40b4365975776e99cd8
Thomas Postak <braves7122@hotmail.com>
2022-04-08 20:10:11 -0400

    Added changes from aikos branch

--- app/src/main/java/com/mentalhealthapp/moody/ProfileFragment.java
+++ app/src/main/java/com/mentalhealthapp/moody/ProfileFragment.java
@@ -1,64 +1,73 @@
 package com.mentalhealthapp.moody;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FirebaseAuth auth;
+    View profileView;
+    private String userName = HomeActivity.userName;
+    TextView helloMessage;
+    ImageView profilePic;
+    Button takePicButton, updatePasswordButton, deleteAccountButton, logOutButton;
+    final String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
+        profileView = view;
+        helloMessage = (TextView) profileView.findViewById(R.id.hello_message);
+        takePicButton = (Button) profileView.findViewById(R.id.take_picture_button);
+        updatePasswordButton = (Button) profileView.findViewById(R.id.update_password_button);
+        deleteAccountButton = (Button) profileView.findViewById(R.id.delete_account_button);
+        logOutButton = (Button) profileView.findViewById(R.id.log_out_button);
+        String welcomeMessage = "Hello " + userName + " !";
+        helloMessage.setText(welcomeMessage);
+        updatePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), UpdatePasswordActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        deleteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), DeleteAccountActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                i.putExtra("Log Out", true);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                getActivity().startActivity(i);
+                auth.signOut();
+                getActivity().finish();
+            }
+        });
     }
 }
