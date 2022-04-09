d804b5c9975abc3fb772d10ad1da7be0ab7b780b
Thomas Postak <braves7122@hotmail.com>
2022-04-08 20:35:50 -0400

    Added changes from aikos branch again. added journalfragment code again

--- app/src/main/java/com/mentalhealthapp/moody/JournalFragment.java
+++ app/src/main/java/com/mentalhealthapp/moody/JournalFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -18,11 +19,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,10 +41,13 @@ import com.google.firebase.database.FirebaseDatabase;
 public class JournalFragment extends Fragment {
     private DatabaseReference db;
     private String userID;
+    List<Journal> journalList;
     View journalFragmentView;
+    Boolean journalFilled = false;
     EditText etJournalTitle, etJournalText;
     Journal journal;
-    Button reviewJournals, submitButton;
+    ImageView previewImg;
+    Button uploadJournalPicture, reviewJournals, submitButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,17 +56,50 @@ public class JournalFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_journal, container, false);
     }
 
+//    ActivityResultLauncher<Intent> launchSelectImage = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+//        @Override
+//        public void onActivityResult(Uri result) {
+//
+//        }
+//    });
+//
+//    public void selectImage(){
+//        Intent iGallery = new Intent(Intent.ACTION_PICK);
+//        iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(iGallery, GALLERY_REQ_CODE);
+//        selectImageIntent.setType("image/*");
+//        selectImageIntent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(Intent.createChooser(selectImageIntent, "Upload Image"), 200);
+//    }
+
     public void createJournal(String journalTitle, String journalText){
         journal = new Journal();
         journal.setJournalTitle(journalTitle);
         journal.setJournalText(journalText);
     }
 
+    public void writeJournalDB(){
+
+    }
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         // Initialize firebase database
         userID = FirebaseAuth.getInstance().getUid();
-        db = FirebaseDatabase.getInstance().getReference();
+        db = FirebaseDatabase.getInstance().getReference("JournalTest/" + userID);
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<HashMap> value = (List) snapshot.getValue();
+                if (value != null){
+                    updateJournalList(value);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         // Get current view
         journalFragmentView = view;
@@ -66,16 +111,59 @@ public class JournalFragment extends Fragment {
         // Initialize all the buttons
         reviewJournals = (Button) journalFragmentView.findViewById(R.id.review_journals);
         submitButton = (Button) journalFragmentView.findViewById(R.id.submit_journal);
-
+        journalList = new ArrayList<>();
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int i = 2;
                 String journalTitle = etJournalTitle.getText().toString();
                 String journalText = etJournalText.getText().toString();
-                createJournal(journalTitle,journalText);
+                if (!(journalTitle.equals("") && journalText.equals(""))) {
+                    Journal journal = new Journal(journalTitle,journalText);
+                    updateDatabase(journal);
+                } else {
+                    Toast.makeText(getView().getContext(), "Please enter a title and description", Toast.LENGTH_SHORT).show();
+                }
             }
         } );
     }
+
+    private void updateDatabase(Journal journal) {
+        List<Journal> updateJournal = new ArrayList<>();
+        for (int i=0; i<journalList.size(); i++){
+            updateJournal.add(journalList.get(i));
+        }
+        updateJournal.add(journal);
+
+        db.setValue(updateJournal);
+    }
+
+    private void updateJournalList(List<HashMap> journalList) {
+        this.journalList.clear();
+        for (int i=0;i< journalList.size();i++){
+            HashMap journal = journalList.get(i);
+            Journal newJournal = new Journal();
+
+            newJournal.setJournalTitle(journal.get("journalTitle").toString());
+            newJournal.setJournalText(journal.get("journalText").toString());
+            newJournal.setDate(journal.get("date").toString());
+
+            this.journalList.add(newJournal);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/mentalhealthapp/moody/ProfileFragment.java
+++ app/src/main/java/com/mentalhealthapp/moody/ProfileFragment.java
@@ -1,5 +1,18 @@
 package com.mentalhealthapp.moody;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,30 +20,95 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ProfileFragment extends Fragment {
+    public static final int CAMERA_PERM_CODE = 101;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentPhotoPath;
     FirebaseAuth auth;
     View profileView;
     private String userName = HomeActivity.userName;
     TextView helloMessage;
     ImageView profilePic;
     Button takePicButton, updatePasswordButton, deleteAccountButton, logOutButton;
-    final String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private  void askCameraPermissions(){
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA},  101 );
+        }else{
+            Toast.makeText(getActivity(), "Permission already granted", Toast.LENGTH_SHORT).show();
+            dispatchTakePictureIntent();
+        }
+    }
+
+    private ActivityResultLauncher<String> permissionResult =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(getActivity(), "Camera Permission Is Required To Use Camera.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    public void dispatchTakePictureIntent(){
+        Toast.makeText(getActivity(), "Camera Permission Is Granted!", Toast.LENGTH_SHORT).show();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
 
     @Override
@@ -44,6 +122,13 @@ public class ProfileFragment extends Fragment {
         logOutButton = (Button) profileView.findViewById(R.id.log_out_button);
         String welcomeMessage = "Hello " + userName + " !";
         helloMessage.setText(welcomeMessage);
+        takePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askCameraPermissions();
+                permissionResult.launch(Manifest.permission.CAMERA);
+            }
+        });
         updatePasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,4 +155,6 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
+
+
 }
